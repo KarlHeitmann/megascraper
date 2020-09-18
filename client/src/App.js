c65dc088880b5eb012a000c45a15460e14e7d1ce
Karl Heitmann <khf1988@gmail.com>
2020-09-18 19:12:07 -0300

    agregado llamada a localhost:4000 para obtener los datos de las motos, y agregado la tabla con columnas, todo fake

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
   Layout,
+  Table,
 } from 'antd';
+import axios from 'axios';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-
 
 const {
   Footer,
@@ -14,10 +15,54 @@ const {
 } = Layout;
 
 function App() {
+
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+
+  useEffect(() => {
+    console.log("useEffect");
+    const url_request = `http://localhost:4000/`;
+    axios.get(url_request)
+      .then(response => {
+        console.log(response);
+      })
+  }, [])
   return (
     <Layout>
       <Header>Header</Header>
-      <Content>Content</Content>
+      <Content>
+        <Table dataSource={dataSource} columns={columns} />;
+      </Content>
       <Footer>Footer</Footer>
     </Layout>
   );
