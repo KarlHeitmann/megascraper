3bd32224a7f6c25c7999f6c18403cd42532b6ec4
Karl Heitmann <khf1988@gmail.com>
2020-09-19 02:24:46 -0300

    agregado el bot

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,30 @@ require('dotenv').config()
 const yapo_motos = require('./scrapers/yapo_motos');
 const workana_job = require('./scrapers/workana_job');
 
+const TelegramBot = require('node-telegram-bot-api');
+const token_bot = process.env.SCRAPERO_BOT_KEY;
+const bot = new TelegramBot(token_bot, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
+
+
 const cors = require("cors");
 const app = express();
 app.use(cors());
