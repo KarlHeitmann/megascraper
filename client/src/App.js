4c88711ae99b2d0c48ab02e8f974480690f98ee9
Karl Heitmann <khf1988@gmail.com>
2020-09-18 23:11:25 -0300

    Archivo pages/motos.jsx contiene la logica de la moto

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {
   Layout,
-  Table,
 } from 'antd';
-import axios from 'axios';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import Motos from './pages/motos';
 
@@ -17,107 +15,11 @@ const {
 
 function App() {
 
-  const [motos, setMotos] = useState([]);
-
-  const columns = [
-    {
-      title: 'Titulo',
-      dataIndex: 'titulo',
-      key: 'titulo',
-    },
-    {
-      title: 'URL',
-      dataIndex: 'url',
-      key: 'url',
-      render: (text, row, index) => {
-        return <a href={text}>Ir</a>
-        // if (index < 4) {
-        //   return <a>{text}</a>;
-        // }
-        // return {
-        //   children: <a>{text}</a>,
-        //   props: {
-        //     colSpan: 5,
-        //   },
-        // };
-      },
-    },
-    {
-      title: 'precio',
-      dataIndex: 'precio',
-      key: 'precio',
-      sortDirections: ['ascend', 'descend'],
-      sorter: (a, b) => Number(a.precio) > Number(b.precio),
-      render: (text, row, index) => {
-        // let salida = []
-        // let partida = true
-        // for (let i=(text.length - 1); i>=0; i--) {
-        //   if (partida) {
-        //     partida = false
-        //   } else {
-        //     if ((i%3) == 0) {
-        //       salida.unshift(".")
-        //     }
-        //   }
-        //   salida.unshift(text[i])
-        // }
-
-        // return salida.join("");
-        return text
-      }
-      
-    },
-    {
-      title: 'Año',
-      dataIndex: 'year',
-      key: 'year',
-    },
-    {
-      title: 'Kilometraje',
-      dataIndex: 'kilometraje',
-      key: 'kilometraje',
-    },
-    {
-      title: 'Ciudad',
-      dataIndex: 'ciudad',
-      key: 'ciudad',
-    },
-    {
-      title: 'Fecha',
-      dataIndex: 'fecha',
-      key: 'fecha',
-    },
-    {
-      title: 'Descripciòn',
-      dataIndex: 'descripcion',
-      key: 'descripcion',
-    },
-    {
-      title: 'Cilindrada',
-      dataIndex: 'cilindrada',
-      key: 'cilindrada',
-      sortDirections: ['ascend', 'descend'],
-      sorter: (a, b) => Number(a.cilindrada) > Number(b.cilindrada),
-    },
-  ];
-
-  useEffect(() => {
-    console.log("useEffect");
-    const url_request = `http://localhost:4000/`;
-    axios.get(url_request)
-      .then(response => {
-        console.log(response);
-        const { motos } = response.data;
-        console.log(motos)
-        setMotos( motos )
-      })
-  }, [])
   return (
     <Layout>
       <Header>Header</Header>
       <Content>
         <Motos/>
-        <Table dataSource={motos} columns={columns} />;
       </Content>
       <Footer>Footer</Footer>
     </Layout>
--- client/src/pages/motos.jsx
+++ client/src/pages/motos.jsx
@@ -1,8 +1,107 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {
+  Table,
+} from 'antd';
+import axios from 'axios';
 
 function Motos(props) {
+  const [motos, setMotos] = useState([]);
+  const columns = [
+    {
+      title: 'Titulo',
+      dataIndex: 'titulo',
+      key: 'titulo',
+    },
+    {
+      title: 'URL',
+      dataIndex: 'url',
+      key: 'url',
+      render: (text, row, index) => {
+        return <a href={text}>Ir</a>
+        // if (index < 4) {
+        //   return <a>{text}</a>;
+        // }
+        // return {
+        //   children: <a>{text}</a>,
+        //   props: {
+        //     colSpan: 5,
+        //   },
+        // };
+      },
+    },
+    {
+      title: 'precio',
+      dataIndex: 'precio',
+      key: 'precio',
+      sortDirections: ['ascend', 'descend'],
+      sorter: (a, b) => Number(a.precio) > Number(b.precio),
+      render: (text, row, index) => {
+        // let salida = []
+        // let partida = true
+        // for (let i=(text.length - 1); i>=0; i--) {
+        //   if (partida) {
+        //     partida = false
+        //   } else {
+        //     if ((i%3) == 0) {
+        //       salida.unshift(".")
+        //     }
+        //   }
+        //   salida.unshift(text[i])
+        // }
+
+        // return salida.join("");
+        return text
+      }
+      
+    },
+    {
+      title: 'Año',
+      dataIndex: 'year',
+      key: 'year',
+    },
+    {
+      title: 'Kilometraje',
+      dataIndex: 'kilometraje',
+      key: 'kilometraje',
+    },
+    {
+      title: 'Ciudad',
+      dataIndex: 'ciudad',
+      key: 'ciudad',
+    },
+    {
+      title: 'Fecha',
+      dataIndex: 'fecha',
+      key: 'fecha',
+    },
+    {
+      title: 'Descripciòn',
+      dataIndex: 'descripcion',
+      key: 'descripcion',
+    },
+    {
+      title: 'Cilindrada',
+      dataIndex: 'cilindrada',
+      key: 'cilindrada',
+      sortDirections: ['ascend', 'descend'],
+      sorter: (a, b) => Number(a.cilindrada) > Number(b.cilindrada),
+    },
+  ];
+
+  useEffect(() => {
+    console.log("useEffect");
+    const url_request = `http://localhost:4000/`;
+    axios.get(url_request)
+      .then(response => {
+        console.log(response);
+        const { motos } = response.data;
+        console.log(motos)
+        setMotos( motos )
+      })
+  }, [])
+
   return(
-    <h1 className="title">Motos</h1>
+    <Table dataSource={motos} columns={columns} />
   )
 }
 
