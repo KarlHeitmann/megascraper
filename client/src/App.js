1dcac2f0176bf71e2a0775fc708d05b7211a41c1
Karl Heitmann <khf1988@gmail.com>
2020-09-18 23:45:34 -0300

    separado sidebar de motos a su propio sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ import {
   Menu,
 } from 'antd';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import Motos from './pages/motos';
+import MotosSidebar from './sidebar/motos_sidebar';
 
 const {
   Footer,
@@ -41,38 +41,14 @@ function App() {
         <Sider>
           <Switch>
             <Route path="/">
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%', borderRight: 0 }}
-                >
-                <Menu.SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </Menu.SubMenu>
-                <Menu.SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </Menu.SubMenu>
-                <Menu.SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </Menu.SubMenu>
-              </Menu>
+              <MotosSidebar/>
             </Route>
           </Switch>
         </Sider>
         <Content>
           <Switch>
             <Route path="/">
-              {/* <Motos/> */}
+              <Motos/>
             </Route>
           </Switch>
         </Content>
