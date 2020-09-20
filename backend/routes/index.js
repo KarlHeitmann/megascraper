bec7fe2964ef00a9f0be7b7156d2884bbd9f5d2c
Karl Heitmann <khf1988@gmail.com>
2020-09-20 02:56:17 -0300

    llevado codigo controlador a archivo index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,31 @@
 // routes/index.js
 const router = require('express').Router();
 const workanajobsRoutes = require('./workanajobs');
+const workana_job = require('../scrapers/workana_job');
 const path = require('path');
 
 // API routes
 // router.use('/api/books', workanajobsRoutes);
 router.use('/api/workana', workanajobsRoutes);
+
+router.use('/scraper/workana', async(req, res, next) => {
+  const pages = req.query.pages;
+  console.log(pages);
+
+  const vueltas = pages ? Number(pages) : 1;
+  
+  let workana_jobs = [];
+  for (let i = 1; i <= vueltas; i++) {
+    const workana_url = `https://www.workana.com/jobs?category=it-programming&language=es&page=${i}`;
+    const { scrapedJobs } = await workana_job.scrapePage(workana_url);
+    workana_jobs = workana_jobs.concat(scrapedJobs);
+    // console.log(workana_jobs);
+  }
+  workana_job.insertWorkanaJobInMongoDb(workana_jobs);
+  // console.log(workana_jobs)
+  // bot.sendMessage(861511144, workana_jobs[0].titulo);
+  res.send({workana_jobs});
+})
 
 // If no API routes are hit, send the React app
 // router.use(function(req, res) {
