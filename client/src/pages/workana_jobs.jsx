9005026c9a0562d943c2a3f0743996d677cfcacb
Karl Heitmann <khf1988@gmail.com>
2020-09-19 23:56:49 -0300

    agregado formulario copy paste

--- client/src/pages/workana_jobs.jsx
+++ client/src/pages/workana_jobs.jsx
@@ -2,10 +2,22 @@ import React, {useState} from 'react';
 import {
   Table,
   Button,
+  Row,
+  Col,
+  Input,
+  Form, Checkbox
 } from 'antd';
 import axios from 'axios';
 
 import workana_jobs_columns from '../table_columnas/workana_jobs';
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 function WorkanaJobs(props) {
   const [workana_jobs, setWorkanaJobs] = useState([])
@@ -22,16 +34,76 @@ function WorkanaJobs(props) {
       })
   }
 
+  const onInputChange = (text) => {
+    console.log(text)
+  }
+
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
   return(
     <>
       <h1 className="title">Workana</h1>
-      <Button
-        type="primary"
-        onClick={onBtnScrape}
-        >
-        Scrape
-      </Button>
-      <Table dataSource={workana_jobs} columns={workana_jobs_columns} />
+      <Row>
+        <Col>
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+          {/* <Input
+            addonBefore="https://"
+            addonAfter=".com"
+            defaultValue="workana"
+            onChange={onInputChange}
+            />
+          <Button
+            type="primary"
+            onClick={onBtnScrape}
+            >
+            Scrape
+          </Button> */}
+
+        </Col>
+      </Row>
+      {/* <Row>
+        <Col> */}
+          <Table dataSource={workana_jobs} columns={workana_jobs_columns} />
+        {/* </Col>
+      </Row> */}
     </>
   )
 }
