b4b56facfa7cbdff0ef6b807b0d2120f1467e4ec
Karl Heitmann <khf1988@gmail.com>
2020-09-21 00:43:57 -0300

    agregado los archivos incluso build/ para meterlo al github y subirlo a heroku

---
+++ client/build/precache-manifest.f09b3ea3b8b8779a933367e1b9911daf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08138d089ece2f87e16d3f13456b0cc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a1f4ea5dca5952c8b9a",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "f1d0ca39cb423b667a52",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4a1f4ea5dca5952c8b9a",
+    "url": "/static/js/2.e4dd7a88.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e4dd7a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d0ca39cb423b667a52",
+    "url": "/static/js/main.f7faa4d3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
