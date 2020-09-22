c32ce62ba8b88a78a480e7e38068ad3357db589e
Karl Heitmann <khf1988@gmail.com>
2020-09-21 23:56:10 -0300

    modificado filtrarScraper para pillar noticias que lleven deshabilitado como null

--- backend/models/WorkanaJob.js
+++ backend/models/WorkanaJob.js
@@ -8,19 +8,38 @@ const workanaJobSchema = new mongoose.Schema({
   descripcion: String,
   deadline: String,
   propuestas: String,
+  deshabilitado: {
+    type: Boolean,
+    default: false
+  },
 });
 // WorkanaJob.statics.findByName = function (name, cb) {
 workanaJobSchema.statics.filtrarScraper = function () {
   return this.find(
-    {
-      $or: [
-        { "descripcion" : { $regex: /.*scrap.*/, $options: 'i' } },
-        { "descripcion" : { $regex: /.*extraer.*/, $options: 'i' } },
-        { "titulo" : { $regex: /.*scrap.*/, $options: 'i' } },
-        { "titulo" : { $regex: /.*extraer.*/, $options: 'i' } },
+    { 
+      $and: [
+        { deshabilitado: null },
+        {
+          $or: [
+            { "descripcion" : { $regex: /.*scrap.*/, $options: 'i' } },
+            { "descripcion" : { $regex: /.*extraer.*/, $options: 'i' } },
+            { "titulo" : { $regex: /.*scrap.*/, $options: 'i' } },
+            { "titulo" : { $regex: /.*extraer.*/, $options: 'i' } },
+          ]
+        }
       ]
     }
   );
+  // return this.find(
+  //   {
+  //     $or: [
+  //       { "descripcion" : { $regex: /.*scrap.*/, $options: 'i' } },
+  //       { "descripcion" : { $regex: /.*extraer.*/, $options: 'i' } },
+  //       { "titulo" : { $regex: /.*scrap.*/, $options: 'i' } },
+  //       { "titulo" : { $regex: /.*extraer.*/, $options: 'i' } },
+  //     ]
+  //   }
+  // );
 }
 
 const WorkanaJob = mongoose.model("WorkanaJob", workanaJobSchema);
