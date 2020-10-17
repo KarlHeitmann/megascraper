bebaf052cc8b56e203b320196adbe6ef7b0beb9f
Karl Heitmann <khf1988@gmail.com>
2020-10-17 00:40:14 -0300

    muestra algunas cuentas en el grafico

--- client/src/pages/workana_grafs.jsx
+++ client/src/pages/workana_grafs.jsx
@@ -27,20 +27,34 @@ function WorkanaGrafs(props) {
   useEffect(() => {
 
     console.log("useEffect");
-    const url_request = `${DOMAIN}/api/workana/filtrar_scraper`;
+    const url_request = `${DOMAIN}/api/workana/grafs_data`;
     // const url_request = `${DOMAIN}/api/workana`;
     axios.get(url_request)
       .then(response => {
         console.log(response)
         const workana_jobs = response.data;
+        const commerce = workana_jobs.filter(r => r.titulo.match(/commerce/gi)).length
+        const scrap    = workana_jobs.filter(r => r.titulo.match(/scrap/gi)).length
+        const laravel  = workana_jobs.filter(r => r.titulo.match(/laravel/gi)).length
+        const python   = workana_jobs.filter(r => r.titulo.match(/python/gi)).length
+        const rails    = workana_jobs.filter(r => r.titulo.match(/rails/gi)).length
+        console.log(commerce, scrap, laravel, python, rails)
         console.log(workana_jobs)
+        setData({
+          labels: [
+            'commerce', 'scrap', 'laravel', 'python', 'rails'
+          ],
+          datasets: [{
+            data: [commerce, scrap, laravel, python, rails]
+          }]
+        })
         // setWorkanaJobs( workana_jobs )
       })
   }, [])
 
   return (
     <>
-      <h1>Work in progress</h1>
+      <h1 className="title">Cuentas</h1>
       <Doughnut data={data}/>
     </>
   );
