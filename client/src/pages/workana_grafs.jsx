6634c1aaa3ae2e305c848ac13304661de6d802c0
Karl Heitmann <khf1988@gmail.com>
2020-10-17 00:21:19 -0300

    data se define en inicio

--- client/src/pages/workana_grafs.jsx
+++ client/src/pages/workana_grafs.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import { DOMAIN } from '../utils';
 import { Doughnut } from 'react-chartjs-2';
 
 function WorkanaGrafs(props) {
-  const data = {
+  const [data, setData] = useState({
     labels: [
       'Red',
       'Green',
@@ -21,7 +23,21 @@ function WorkanaGrafs(props) {
       '#FFCE56'
       ]
     }]
-  };
+  })
+  useEffect(() => {
+
+    console.log("useEffect");
+    const url_request = `${DOMAIN}/api/workana/filtrar_scraper`;
+    // const url_request = `${DOMAIN}/api/workana`;
+    axios.get(url_request)
+      .then(response => {
+        console.log(response)
+        const workana_jobs = response.data;
+        console.log(workana_jobs)
+        // setWorkanaJobs( workana_jobs )
+      })
+  }, [])
+
   return (
     <>
       <h1>Work in progress</h1>
