50a8b1f049d9bd97e02c7cae24c6e3a7fd0cd4ce
Karl Heitmann <khf1988@gmail.com>
2020-10-22 01:27:18 -0300

    agregado mas javascript producto del build

---
+++ client/build/precache-manifest.dc5e00ff3e4ee025413da2aba4def953.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ec45c8b07ed22eac0c9cac9f316e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b2a6be4d87c45bb22a4",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "b09eb8a215cfdfcff0df",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8b2a6be4d87c45bb22a4",
+    "url": "/static/js/2.b6910b34.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/static/js/2.b6910b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09eb8a215cfdfcff0df",
+    "url": "/static/js/main.eec32510.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "dae400f43eff1f4eefb42c2bdcfcecbc",
+    "url": "/static/media/perfil.dae400f4.jpg"
+  }
+]);
