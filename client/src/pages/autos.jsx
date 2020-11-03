2ae1fcaf1ec1833d267c766ccc6d8c6d2c0ee4d2
Karl Heitmann <khf1988@gmail.com>
2020-11-03 00:47:02 -0300

    colocado Form de busqueda

--- client/src/pages/autos.jsx
+++ client/src/pages/autos.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
+import {
+  Row,
+  Col,
+  Form,
+  Input,
+  Button,
+} from 'antd';
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
 function Autos(props) {
+  const onBtnScrape = (values) => {
+  }
+  const onFinishFailed = errorInfo => {
+  }
   return(
-    <h1>Autos</h1>
+    <>
+      <h1>Autos</h1>
+      <Row>
+        <Col>
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onBtnScrape}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Pages to scrap"
+              name="pages"
+              // rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </>
   );
 };
 
